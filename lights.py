c05d3e25070fb83d391561396ea19ed2ac8b00a9
crusaderky <crusaderky@gmail.com>
2026-05-15 21:52:54 +0100

    Write to file

--- lights.py
+++ lights.py
@@ -3,5 +3,8 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument("value", choices=["on", "off"])
 args = parser.parse_args()
-print(f"Lights {args.value}!")
+msg = f"DEMO API: Lights {args.value}!"
+print(msg)
+with open("lights", "w") as fh:
+    fh.write(msg + "\n")
 
